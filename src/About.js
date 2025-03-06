3eddad9434e8be6b490e065c7872356030311b84
salmanagha09 <107588817+salmanagha09@users.noreply.github.com>
2025-03-07 00:57:24 +0100

    Initial commit with React app setup

---
+++ src/About.js
@@ -0,0 +1,8 @@
+// src/components/About.js
+import React from "react";
+
+function About() {
+  return <h1>About Us Page</h1>;
+}
+
+export default About;
---
+++ src/App.js
@@ -0,0 +1,22 @@
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
